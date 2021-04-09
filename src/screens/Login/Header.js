7b2a1fff50dec4a4cd190aaf586dd6d7fb922872
Silvio Coutinho <silviocoutinho@ymail.com>
2021-04-09 10:01:45 -0300

    Create Header in Login

--- src/screens/Login/Header.js
+++ src/screens/Login/Header.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+import { HeaderStyles } from './Styles';
 
 const Header = props => {
   return (
-    <header>
-      <h3>Header Login</h3>
-    </header>
+    <HeaderStyles>
+      <header>
+        <Navbar expand="lg" fixed="top">
+          <Navbar.Brand href="restrito">
+            Portal do Servidor - Câmara Municipal de Jahu
+          </Navbar.Brand>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </Navbar>
+      </header>
+    </HeaderStyles>
   );
 };
-
 export default Header;
