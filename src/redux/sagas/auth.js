fddec97683eeb0f9120f859e770e6fce4f4e946d
Silvio Coutinho <silviocoutinho@ymail.com>
2021-04-09 11:06:32 -0300

    Fix Port API

--- src/redux/sagas/auth.js
+++ src/redux/sagas/auth.js
@@ -1,93 +1,96 @@
-import {takeLatest, all, put } from 'redux-saga/effects';
+import { takeLatest, all, put } from 'redux-saga/effects';
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 import ActionCreators from '../actionCreators';
 
-export function* login(action){
+export function* login(action) {
+  const portAPI = process.env.REACT_APP_PORT_API;
 
-    const portAPI = process.env.REACT_APP_PORT;
-        
-    const url = `http://localhost:${portAPI}`;
-    let token = localStorage.getItem('token');
-    
-    if(token) {
-        const emailUser = jwtDecode(token).email;    
-        if (emailUser !== action.email) {
-            localStorage.removeItem('token');
-            localStorage.removeItem('user');
-            token = null;
-        } 
+  const url = `http://localhost:${portAPI}`;
+  let token = localStorage.getItem('token');
+
+  if (token) {
+    const emailUser = jwtDecode(token).email;
+    if (emailUser !== action.email) {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      token = null;
     }
+  }
 
-    if(!token) {          
-        try {
-            const login = yield axios.post(`${url}/auth/signin`, {
-                fun_email: action.email,          //'adm@mail.com',
-                fun_passwd: action.password,  //'Test3D3Senh@',
-            });            
-            if(login.data.token){
-                token = login.data.token;
-                localStorage.setItem('token', token);
-                const user = jwtDecode(login.data.token);
-                localStorage.setItem('user', user);
-                yield put(ActionCreators.signinSuccess(user));      
-            };
-        } catch(error) {           
-            localStorage.removeItem('token');
-            localStorage.removeItem('user');
-            yield put(ActionCreators.signinFailure(error.response.data.error, error.response.status));
-        }       
-        
-    };      
-};
+  if (!token) {
+    try {
+      const login = yield axios.post(`${url}/auth/signin`, {
+        fun_email: action.email, //'adm@mail.com',
+        fun_passwd: action.password, //'Test3D3Senh@',
+      });
+      if (login.data.token) {
+        token = login.data.token;
+        localStorage.setItem('token', token);
+        const user = jwtDecode(login.data.token);
+        localStorage.setItem('user', user);
+        yield put(ActionCreators.signinSuccess(user));
+      }
+    } catch (error) {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      yield put(
+        ActionCreators.signinFailure(
+          error.response.data.error,
+          error.response.status,
+        ),
+      );
+    }
+  }
+}
 
 export function* validateToken() {
-    const port = 3300;
-    const url = `http://localhost:${port}`;
-    let token = localStorage.getItem('token');
-    
-    if(token) {
-        try {
-            const validateToken = yield axios.post(`${url}/auth/validate/token`, {
-                token: token,                    
-            });
-            const user = jwtDecode(token);
-            if (validateToken.status === 200) {
-                yield put(ActionCreators.validateTokenSuccess(user));
-            } else {
-                localStorage.removeItem('token');
-                localStorage.removeItem('user');
-                yield put(ActionCreators.validateTokenFailure('Token inválido', 401));  
-            };            
+  const port = 3300;
+  const url = `http://localhost:${port}`;
+  let token = localStorage.getItem('token');
 
-        } catch (error) {
-            localStorage.removeItem('token');
-            localStorage.removeItem('user');
-            yield put(ActionCreators.validateTokenFailure(error.response.data.error, error.response.status));
-        }        
-    };
-
-    
+  if (token) {
+    try {
+      const validateToken = yield axios.post(`${url}/auth/validate/token`, {
+        token: token,
+      });
+      const user = jwtDecode(token);
+      if (validateToken.status === 200) {
+        yield put(ActionCreators.validateTokenSuccess(user));
+      } else {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        yield put(ActionCreators.validateTokenFailure('Token inválido', 401));
+      }
+    } catch (error) {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      yield put(
+        ActionCreators.validateTokenFailure(
+          error.response.data.error,
+          error.response.status,
+        ),
+      );
+    }
+  }
 }
 
 export function* auth() {
-    let token = localStorage.getItem('token');
-    if(token){
-        try {
-            const user = jwtDecode(token);                           
-            yield put(ActionCreators.authSuccess(user));    
-        } catch (error) {
-            yield put(ActionCreators.authFailure('Invalid token'));
-        }
-        
-    } else{
-        yield put(ActionCreators.authFailure('No token'));
+  let token = localStorage.getItem('token');
+  if (token) {
+    try {
+      const user = jwtDecode(token);
+      yield put(ActionCreators.authSuccess(user));
+    } catch (error) {
+      yield put(ActionCreators.authFailure('Invalid token'));
     }
-};
-
-export function* destroyAuth() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    yield put(ActionCreators.destroyAuthSuccess());
+  } else {
+    yield put(ActionCreators.authFailure('No token'));
+  }
 }
 
+export function* destroyAuth() {
+  localStorage.removeItem('token');
+  localStorage.removeItem('user');
+  yield put(ActionCreators.destroyAuthSuccess());
+}
