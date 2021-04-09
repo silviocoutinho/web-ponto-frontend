7321bee9fc85c89be558d0a51559dff0aa3d4459
Silvio Coutinho <silviocoutinho@ymail.com>
2021-04-09 10:00:33 -0300

    Add Router in APP

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
-import { Component } from 'react';
-import axios from 'axios';
-import jwtDecode from 'jwt-decode';
+import React, { Component } from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 import store from './redux';
 import { Provider } from 'react-redux';
 
 import Header from './Header';
+import Login from './screens/Login/index';
 class App extends Component {
-  async componentDidMount() {
-    const url = 'http://localhost:3005';
-    const login = await axios.post(`${url}/auth/signin`, {
-      fun_email: '1617886500713@mail.com',
-      fun_passwd: 'Test3D3Senh@',
-    });
-    console.log(login.data);
-    const decoded = jwtDecode(login.data.token);
-    console.log(decoded);
-  }
-
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <h1>Titulo App</h1>
-          <Header />
-        </div>
+        <Router>
+          <Route exact path="/" component={Login} />
+        </Router>
       </Provider>
     );
   }
