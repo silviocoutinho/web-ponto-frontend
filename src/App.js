b0bd811b1229bd97c90d867e5284ad9ff7835773
Silvio Coutinho <silviofc79@gmail.com>
2021-04-12 18:58:59 -0300

    Add routes in App

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import { Provider } from 'react-redux';
 
 import Header from './Header';
 import Login from './screens/Login/index';
+import Admin from './screens/Admin';
+import Restrito from './screens/Restrito';
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <Router>
           <Route exact path="/" component={Login} />
+          <Route path="/admin" component={Admin} />
+          <Route path="/restrito" component={Restrito} />
           <Route exact path="/login" component={Login} />
         </Router>
       </Provider>
