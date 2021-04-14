14cd9a176d3546b152d4702401517bd89e4208c2
Silvio Coutinho <silviocoutinho@ymail.com>
2021-04-14 11:52:21 -0300

    Add Nav to Menu component

--- src/screens/Restrito/elements/Menu.js
+++ src/screens/Restrito/elements/Menu.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Table, Nav } from 'react-bootstrap';
+
+import { BsFillCaretRightFill as BsCicle } from 'react-icons/bs';
+import { BsGridFill as BsGrid } from 'react-icons/bs';
 
 const Menu = () => {
-  return <h1>Menu</h1>;
+  return (
+    <div>
+      <div className="menu-lateral-titulo">
+        <h5>Menu</h5>
+      </div>
+      <a>
+        <BsGrid />
+        Pontos
+      </a>
+      <Nav defaultActiveKey="/home" className="flex-column">
+        <Nav.Link href="/home">Active</Nav.Link>
+        <Nav.Link eventKey="link-1">Link</Nav.Link>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+        <Nav.Link eventKey="disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav>
+    </div>
+  );
 };
 
 export default Menu;
