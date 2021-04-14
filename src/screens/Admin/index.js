1264de726951ea69379d0a3c30eb2b09db2b8b02
silviocoutinho <silviocoutinho@ymail.com>
2021-04-13 23:49:14 -0300

    Create Admin index page

--- src/screens/Admin/index.js
+++ src/screens/Admin/index.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import { Route, Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Admin = props => <h1>Admin</h1>;
+const Home = props => <h1>Home Admin</h1>;
+const Users = props => <h1>Users Admin</h1>;
 
-export default Admin;
+const Admin = props => {
+  const path = props.match.path;
+  if (!props.auth.isAuth) {
+    return <Redirect to="/login" />;
+  }
+  if (props.auth.user.adm !== true) {
+    return <Redirect to="/restrito" />;
+  }
+  return (
+    <div>
+      <h1>Admin</h1>
+      <h3>{JSON.stringify(props.auth)}</h3>
+      <p>
+        <Link to="/admin">Admin</Link>
+        <Link to="/restrito">Restrito</Link>
+      </p>
+      <div>
+        <Route path={`${path}/`} exact component={Home} />
+        <Route path="/restrito" component={Users} />
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth,
+  };
+};
+
+export default connect(mapStateToProps)(Admin);
