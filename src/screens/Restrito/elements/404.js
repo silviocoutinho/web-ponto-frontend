53d8b126858758df7b936e63756eb7acfc6a37ca
Silvio Coutinho <silviocoutinho@ymail.com>
2021-04-15 11:10:03 -0300

    Create 404 Page

---
+++ src/screens/Restrito/elements/404.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+
+import Header from '../elements/Header';
+import Menu from '../elements/Menu';
+import { IndexStyles } from '../Styles';
+
+const PageNotFound = props => {
+  return (
+    <IndexStyles>
+      <Header />
+      <Container fluid="xs md lg">
+        <Row>
+          <Col xs={4} md={3} className="menu-lateral">
+            <Menu />
+          </Col>
+          <Col xs={8} md={9} className="principal">
+            <h1>Página não encontrada!</h1>
+          </Col>
+        </Row>
+      </Container>
+    </IndexStyles>
+  );
+};
+
+export default PageNotFound;
