f13a957e41502e87e87706ca87acf2d520a3bba1
Silvio Coutinho <silviocoutinho@ymail.com>
2021-04-20 16:38:44 -0300

    Create DatePicker Component

---
+++ src/screens/components/DatePicker/Styles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const ComponentStyles = styled.div`
+  .formulario-jumbotron {
+    margin: 1rem;
+  }
+
+  .formulario-info {
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+
+  .formulario-info-ano {
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin-left: 1.5rem;
+    text-align: left;
+  }
+
+  .formulario-badge {
+    margin-left: 0.5rem;
+    width: 7rem;
+  }
+
+  .formulario-item {
+    margin-top: 1.5rem;
+    font-size: 1.25rem;
+    font-weight: bold;
+  }
+
+  .formulario-button {
+    width: 10rem;
+    margin: 0.5rem;
+  }
+
+  .formulario-aviso-erro {
+    width: 35rem;
+  }
+`;
