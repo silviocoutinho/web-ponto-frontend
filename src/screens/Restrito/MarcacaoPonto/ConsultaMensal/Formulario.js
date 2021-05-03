eac5980cf42f716b4dcc496067dfbb1a0b0ccf10
silviocoutinho <silviocoutinho@ymail.com>
2021-05-03 12:03:54 -0300

    Add Input component from lib components-ui-cmjau

--- src/screens/Restrito/MarcacaoPonto/ConsultaMensal/Formulario.js
+++ src/screens/Restrito/MarcacaoPonto/ConsultaMensal/Formulario.js
@@ -3,6 +3,8 @@ import { Container, Form, Button, Jumbotron, Badge } from 'react-bootstrap';
 
 import { IndexStyles } from '../../Styles';
 
+import { Input } from 'components-ui-cmjau';
+
 import SelectMonth from './../../../components/SelectMonth';
 
 const Formulario = props => {
@@ -13,6 +15,11 @@ const Formulario = props => {
         <Jumbotron className="formulario-jumbotron shadow p-5 mb-1 rounded">
           <Form>
             <SelectMonth label="Selecione o Mês" placeholder="Digite o valor" />
+            <Input
+              fieldName="example1"
+              label="Example"
+              placeholder="Type a text"
+            />
             <Button
               variant="primary"
               type="submit"
