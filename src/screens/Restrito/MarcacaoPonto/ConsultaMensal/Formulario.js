d92f59096a4de6903598f3a8766261ca4b223387
silviocoutinho <silviocoutinho@ymail.com>
2021-05-07 17:12:58 -0300

    Add Formik for Test

--- src/screens/Restrito/MarcacaoPonto/ConsultaMensal/Formulario.js
+++ src/screens/Restrito/MarcacaoPonto/ConsultaMensal/Formulario.js
@@ -1,11 +1,23 @@
-import React, { useEffect } from 'react';
-import { Container, Form, Button, Jumbotron, Badge } from 'react-bootstrap';
+import React from 'react';
+import { Container, Jumbotron, Badge } from 'react-bootstrap';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 import { IndexStyles } from '../../Styles';
 
 import { Input } from 'components-ui-cmjau';
 
-import SelectMonth from './../../../components/SelectMonth';
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+});
 
 const Formulario = props => {
   return (
@@ -13,24 +25,59 @@ const Formulario = props => {
       <h1>Relatório por mês</h1>
       <Container className="meio">
         <Jumbotron className="formulario-jumbotron shadow p-5 mb-1 rounded">
-          <Form>
-            <SelectMonth label="Selecione o Mês" placeholder="Digite o valor" />
-            <Input
-              fieldName="example1"
-              label="Example"
-              placeholder="Type a text"
-            />
-            <Button
-              variant="primary"
-              type="submit"
-              className="formulario-button"
-            >
-              Enviar
-            </Button>
-            <Button variant="success" className="formulario-button">
-              Limpar
-            </Button>
-          </Form>
+          <Formik
+            initialValues={{
+              firstName: '',
+
+              lastName: '',
+
+              email: '',
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={values => {
+              // same shape as initial values
+
+              console.log(values);
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form>
+                <Field
+                  name="firstName"
+                  as={Input}
+                  label="First Name"
+                  placeholder="teste"
+                  errorMessage={
+                    errors.firstName && touched.firstName
+                      ? errors.firstName
+                      : null
+                  }
+                />
+                <Field
+                  name="lastName"
+                  as={Input}
+                  label="Last Name"
+                  placeholder="teste"
+                  errorMessage={
+                    errors.lastName && touched.lastName ? errors.lastName : null
+                  }
+                />
+
+                <Field
+                  name="email"
+                  type="email"
+                  as={Input}
+                  label="E-mail"
+                  placeholder="Your E-mail"
+                  errorMessage={
+                    errors.email && touched.email ? errors.email : null
+                  }
+                />
+
+                <button type="submit">Submit</button>
+              </Form>
+            )}
+          </Formik>
         </Jumbotron>
       </Container>
     </IndexStyles>
