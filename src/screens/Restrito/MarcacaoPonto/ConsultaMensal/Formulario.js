2dd14c8cf0747df655f76cd77bc06615409ee9a9
Silvio Coutinho <silviocoutinho@ymail.com>
2021-05-20 17:11:49 -0300

    Test Select Component

--- src/screens/Restrito/MarcacaoPonto/ConsultaMensal/Formulario.js
+++ src/screens/Restrito/MarcacaoPonto/ConsultaMensal/Formulario.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import { Container, Jumbotron, Badge } from 'react-bootstrap';
+import { Container, Jumbotron } from 'react-bootstrap';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
 import { IndexStyles } from '../../Styles';
 
-import { Input, Alert, Button } from 'components-ui-cmjau';
+import { Alert, Button, Input, Select } from 'components-ui-cmjau';
+
+const dataTimeCard = [
+  {
+    dia: '2021-05-10',
+    ent1: '08:01',
+    sai1: '12:10',
+    ent2: '13:30',
+    sai2: '17:00',
+    ent3: null,
+    sai3: null,
+  },
+];
+
+const months = new Array(
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Agosto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro',
+);
 
 const SignupSchema = Yup.object().shape({
   firstName: Yup.string()
@@ -16,7 +43,6 @@ const SignupSchema = Yup.object().shape({
     .min(2, 'Too Short!')
     .max(50, 'Too Long!')
     .required('Required'),
-  email: Yup.string().email('Invalid email').required('Required'),
 });
 
 const Formulario = props => {
@@ -24,58 +50,56 @@ const Formulario = props => {
     <IndexStyles>
       <h1>Relatório por mês</h1>
       <Container className="meio">
-        <Jumbotron className="formulario-jumbotron shadow p-5 mb-1 rounded">
-          <Formik
-            initialValues={{
-              firstName: '',
-              lastName: '',
-              email: '',
-            }}
-            validationSchema={SignupSchema}
-            onSubmit={values => {
-              console.log(values);
-            }}
-          >
-            {({ errors, touched }) => (
-              <Form>
-                <Alert message="teste" type="danger" />
-                <Field
-                  name="firstName"
-                  as={Input}
-                  label="First Name"
-                  placeholder="teste"
-                  errorMessage={
-                    errors.firstName && touched.firstName
-                      ? errors.firstName
-                      : null
-                  }
-                />
-                <Field
-                  name="lastName"
-                  as={Input}
-                  label="Last Name"
-                  placeholder="teste"
-                  errorMessage={
-                    errors.lastName && touched.lastName ? errors.lastName : null
-                  }
-                />
+        <Formik
+          initialValues={{
+            firstName: '',
+            lastName: '',
+            email: '',
+          }}
+          validationSchema={SignupSchema}
+          onSubmit={values => {
+            console.log(values);
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <Alert message="teste" type="danger" />
+              <Field
+                name="firstName"
+                as={Input}
+                label="First Name"
+                placeholder="teste"
+                errorMessage={
+                  errors.firstName && touched.firstName
+                    ? errors.firstName
+                    : null
+                }
+              />
+              <Field
+                name="lastName"
+                as={Input}
+                label="Last Name"
+                placeholder="teste"
+                errorMessage={
+                  errors.lastName && touched.lastName ? errors.lastName : null
+                }
+              />
 
-                <Field
-                  name="email"
-                  type="email"
-                  as={Input}
-                  label="E-mail"
-                  placeholder="Your E-mail"
-                  errorMessage={
-                    errors.email && touched.email ? errors.email : null
-                  }
-                />
+              <Field
+                name="email"
+                as={Select}
+                label="E-mail"
+                field="email"
+                data={months}
+                errorMessage={
+                  errors.email && touched.email ? errors.email : null
+                }
+              />
 
-                <Button type="submit" label="Enviar" btnStyle="danger" />
-              </Form>
-            )}
-          </Formik>
-        </Jumbotron>
+              <Button type="submit" label="Enviar" btnStyle="danger" />
+            </Form>
+          )}
+        </Formik>
       </Container>
     </IndexStyles>
   );
