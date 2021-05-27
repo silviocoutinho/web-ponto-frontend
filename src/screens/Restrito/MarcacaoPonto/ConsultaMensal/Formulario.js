5b7b419fed7663950625f7a4c3dfeef6d6952d42
Silvio Coutinho <silviocoutinho@ymail.com>
2021-05-27 14:14:12 -0300

    Created structure for Month Report

--- src/screens/Restrito/MarcacaoPonto/ConsultaMensal/Formulario.js
+++ src/screens/Restrito/MarcacaoPonto/ConsultaMensal/Formulario.js
@@ -1,105 +1,120 @@
-import React from 'react';
-import { Container, Jumbotron } from 'react-bootstrap';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import {
+  Alert,
+  Button,
+  Select,
+  GridContainer,
+  TimeCard,
+} from 'components-ui-cmjau';
 
 import { IndexStyles } from '../../Styles';
-
-import { Alert, Button, Input, Select } from 'components-ui-cmjau';
-
-const dataTimeCard = [
-  {
-    dia: '2021-05-10',
-    ent1: '08:01',
-    sai1: '12:10',
-    ent2: '13:30',
-    sai2: '17:00',
-    ent3: null,
-    sai3: null,
-  },
-];
-
-const months = new Array(
-  'Janeiro',
-  'Fevereiro',
-  'Março',
-  'Abril',
-  'Maio',
-  'Junho',
-  'Julho',
-  'Agosto',
-  'Setembro',
-  'Outubro',
-  'Novembro',
-  'Dezembro',
-);
-
-const SignupSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  lastName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-});
+import { months, years } from './Dados';
 
 const Formulario = props => {
+  const [dataTimeCard, setDataTimeCard] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [fieldMonth, setFieldMonth] = useState(1);
+  const [fieldYear, setFieldYear] = useState('1');
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth() + 1;
+
+  const handleYear = evt => {
+    setFieldYear(evt.target.value);
+    setFieldMonth(1);
+  };
+
+  const handleMonth = evt => {
+    setFieldMonth(evt.target.value);
+  };
+
+  const filterData = () => {
+    let selectedYears;
+    years.forEach((element, key) => {
+      if (element.value === fieldYear) selectedYears = element.label;
+    });
+    if (
+      currentYear === Number(selectedYears) &&
+      Number(fieldMonth) > currentMonth
+    ) {
+      setDataTimeCard(null);
+      setErrorMessage(
+        'Mês selecionado inválido. Selecione um mês igual ou inferior ao atual!',
+      );
+    } else {
+      setDataTimeCard([
+        {
+          dia: '2021-05-10',
+          ent1: '08:01',
+          sai1: '12:10',
+          ent2: '13:30',
+          sai2: '17:00',
+          ent3: null,
+          sai3: null,
+        },
+        {
+          dia: '2021-05-11',
+          ent1: '08:00',
+          sai1: '12:00',
+          ent2: '13:30',
+          sai2: '17:30',
+          ent3: null,
+          sai3: null,
+        },
+      ]);
+    }
+  };
+
   return (
     <IndexStyles>
       <h1>Relatório por mês</h1>
       <Container className="meio">
-        <Formik
-          initialValues={{
-            firstName: '',
-            lastName: '',
-            email: '',
-          }}
-          validationSchema={SignupSchema}
-          onSubmit={values => {
-            console.log(values);
-          }}
-        >
-          {({ errors, touched }) => (
-            <Form>
-              <Alert message="teste" type="danger" />
-              <Field
-                name="firstName"
-                as={Input}
-                label="First Name"
-                placeholder="teste"
-                errorMessage={
-                  errors.firstName && touched.firstName
-                    ? errors.firstName
-                    : null
-                }
-              />
-              <Field
-                name="lastName"
-                as={Input}
-                label="Last Name"
-                placeholder="teste"
-                errorMessage={
-                  errors.lastName && touched.lastName ? errors.lastName : null
-                }
-              />
-
-              <Field
-                name="email"
-                as={Select}
-                label="E-mail"
-                field="email"
-                data={months}
-                errorMessage={
-                  errors.email && touched.email ? errors.email : null
-                }
-              />
-
-              <Button type="submit" label="Enviar" btnStyle="danger" />
-            </Form>
-          )}
-        </Formik>
+        <GridContainer columns={3}>
+          <div className="select-container">
+            <Select
+              data={months}
+              field="month"
+              label="Mês"
+              onChange={handleMonth}
+            />
+          </div>
+          <div className="select-container">
+            <Select
+              data={years}
+              field="year"
+              label="Ano"
+              onChange={handleYear}
+            />
+          </div>
+          <div className="button-container">
+            <Button
+              label="Filtrar"
+              btnStyle="primary"
+              type="button"
+              onClick={filterData}
+            />
+          </div>
+        </GridContainer>
+        {dataTimeCard && (
+          <TimeCard
+            data={dataTimeCard}
+            head={{
+              data: 'Dia',
+              dia: 'Data',
+              diaDaSemana: 'Dia da Semana',
+              ent1: 'Entrada 1',
+              ent2: 'Entrada 2',
+              ent3: 'Entrada 3',
+              horaExtra: 'Hora-extra',
+              sai1: 'Saída 1',
+              sai2: 'Saída 2',
+              sai3: 'Saída 3',
+              total: 'Total',
+            }}
+            workingTime={8}
+          />
+        )}
+        {errorMessage && <Alert message={errorMessage} type="danger" />}
       </Container>
     </IndexStyles>
   );
