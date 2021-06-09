89ba1eda652c9de8192364695ff9ddb2dbab23be
Silvio Coutinho <silviocoutinho@ymail.com>
2021-06-09 16:06:18 -0300

    Finish TimeCard my Month

--- src/screens/Restrito/MarcacaoPonto/ConsultaMensal/Formulario.js
+++ src/screens/Restrito/MarcacaoPonto/ConsultaMensal/Formulario.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import axios from 'axios';
-import jwtDecode from 'jwt-decode';
+import toArray from 'lodash/toArray';
 import {
   Alert,
   Button,
@@ -11,13 +11,15 @@ import {
 } from 'components-ui-cmjau';
 
 import { IndexStyles } from '../../Styles';
-import { months, years } from './Dados';
+import { months, years, headTimeCard } from './Dados';
+import { head } from 'lodash';
 
 const Formulario = props => {
   const [dataTimeCard, setDataTimeCard] = useState(null);
   const [errorMessage, setErrorMessage] = useState('');
+  const [typeOfErrorMessage, setTypeOfErrorMessage] = useState('danger');
   const [fieldMonth, setFieldMonth] = useState(1);
-  const [fieldYear, setFieldYear] = useState('1');
+  const [fieldYear, setFieldYear] = useState(1);
   const currentYear = new Date().getFullYear();
   const currentMonth = new Date().getMonth() + 1;
 
@@ -30,39 +32,52 @@ const Formulario = props => {
   const apiURL = `${REACT_APP_URL_API}:${REACT_APP_PORT_API}/${MAIN_ROUTE}`;
 
   const handleYear = evt => {
-    setFieldYear(evt.target.value);
-    setFieldMonth(1);
+    setFieldYear(evt.target.value);    
   };
 
   const handleMonth = evt => {
     setFieldMonth(evt.target.value);
   };
 
-  const getDataFromAPI = () => {
+  const getDataFromAPI = (selectedMonth, selectedYear) => {
     const token = localStorage.getItem('token');
-    const config = {
+    const configHeadersAPI = {
       Authorization: 'Bearer ' + token,
     };
     try {
+      
       axios
         .get(apiURL, {
           params: {
-            year: 2021,
-            month: 5,
+            month: selectedMonth,
+            year: selectedYear,
           },
-          headers: config,
+          headers: configHeadersAPI,
         })
-        .then(res => {
-          console.log(res);
+        .then(res => {      
+          if (res.data.length === 0)
+          {
+            setTypeOfErrorMessage('warning');
+            setErrorMessage('Não existem informações para o período consultado!');
+            setDataTimeCard(null);
+            return;
+          }
+          const arrayDataToTimeCard = toArray(res.data);
+          setErrorMessage(null);
+          setDataTimeCard(arrayDataToTimeCard);
         })
-        .catch(err => {
+        .catch(err => {          
+          setDataTimeCard(null);
+
           if (!err.response) {
+            setTypeOfErrorMessage('danger');
             setErrorMessage(
               'O Servidor está indisponível, contate o Setor de TI!',
             );
           } else {
             const code = err.response.status;
             const response = err.response.data;
+            setTypeOfErrorMessage('danger');
             setErrorMessage(
               `Estamos com problemas no Servidor. Mensagem: ${response}. Código: ${code}`,
             );
@@ -74,40 +89,20 @@ const Formulario = props => {
   };
 
   const filterData = () => {
-    let selectedYears;
-    years.forEach((element, key) => {
-      if (element.value === fieldYear) selectedYears = element.label;
-    });
+    const selectedYears = years.filter(element => Number(element.value) === Number(fieldYear)).map(element => element.label);
+    console.log(selectedYears[0]);
+
     if (
       currentYear === Number(selectedYears) &&
       Number(fieldMonth) > currentMonth
     ) {
       setDataTimeCard(null);
+      setTypeOfErrorMessage('danger');
       setErrorMessage(
         'Mês selecionado inválido. Selecione um mês igual ou inferior ao atual!',
       );
-    } else {
-      setDataTimeCard([
-        {
-          dia: '2021-05-10',
-          ent1: '08:01',
-          sai1: '12:10',
-          ent2: '13:30',
-          sai2: '17:00',
-          ent3: null,
-          sai3: null,
-        },
-        {
-          dia: '2021-05-11',
-          ent1: '08:00',
-          sai1: '12:00',
-          ent2: '13:30',
-          sai2: '17:30',
-          ent3: null,
-          sai3: null,
-        },
-      ]);
-      getDataFromAPI();
+    } else {     
+      getDataFromAPI(Number(fieldMonth), selectedYears);
     }
   };
 
@@ -144,23 +139,11 @@ const Formulario = props => {
         {dataTimeCard && (
           <TimeCard
             data={dataTimeCard}
-            head={{
-              data: 'Dia',
-              dia: 'Data',
-              diaDaSemana: 'Dia da Semana',
-              ent1: 'Entrada 1',
-              ent2: 'Entrada 2',
-              ent3: 'Entrada 3',
-              horaExtra: 'Hora-extra',
-              sai1: 'Saída 1',
-              sai2: 'Saída 2',
-              sai3: 'Saída 3',
-              total: 'Total',
-            }}
+            head={headTimeCard}
             workingTime={8}
           />
         )}
-        {errorMessage && <Alert message={errorMessage} type="danger" />}
+        {errorMessage && <Alert message={errorMessage} type={typeOfErrorMessage} />}
       </Container>
     </IndexStyles>
   );
