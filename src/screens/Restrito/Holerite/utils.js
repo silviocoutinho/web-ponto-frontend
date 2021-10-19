5d0573eb426fa68f61e0d10e0cb288c070e28c3e
silviocoutinho <silviocoutinho@ymail.com>
2021-10-19 08:20:38 -0300

    Add util func to payslip form

---
+++ src/screens/Restrito/Holerite/utils.js
@@ -0,0 +1,15 @@
+const getLabelYearFromArray = (years, yearFromSelect) => {
+  const result = years.filter(arrayOfYears => {
+    return arrayOfYears.value === yearFromSelect.toString();
+  });
+  return result[0].label;
+};
+
+const getIndexMonthFromArray = (months, fieldMonth) => {
+  const result = months.filter(arrayOfYears => {
+    return arrayOfYears.label === fieldMonth.toString();
+  });
+  return result[0].value;
+};
+
+export { getLabelYearFromArray, getIndexMonthFromArray };
