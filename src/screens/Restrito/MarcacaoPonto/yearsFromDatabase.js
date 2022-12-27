9a78ce9e18d3ecf2c11f6dfbb3e4982b3c1c1f92
Silvio Coutinho <silviocoutinho@ymail.com>
2022-12-27 10:01:43 -0300

    feat: add func to get field year from api

---
+++ src/screens/Restrito/MarcacaoPonto/yearsFromDatabase.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const {
+  REACT_APP_PORT_API,
+  REACT_APP_URL_API,
+  REACT_APP_VERSION_API,
+  REACT_APP_ENV,
+} = process.env;
+
+const MAIN_ROUTE = `exercicio-holerites/`;
+const envURL = REACT_APP_ENV === 'test' ? '' : 'http://';
+const baseURL = `${envURL}${REACT_APP_URL_API}:${REACT_APP_PORT_API}/${MAIN_ROUTE}`;
+const token = localStorage.getItem('token');
+const configHeadersAPI = {
+  Authorization: 'Bearer ' + token,
+};
+axios.defaults.baseURL = baseURL;
+
+export const yearsFromDatabase = async setYears => {
+  let response;
+  //, { headers: configHeadersAPI }
+  try {
+    response = await axios.get(`${baseURL}`);
+    console.log(response?.data);
+    setYears(response?.data);
+  } catch (error) {
+    setYears(null);
+    throw new Error(error.message);
+  }
+};
