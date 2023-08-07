8e7ec9f995d3f6e84b3deee72e6c6fc62fe84089
Silvio Ferreira Coutinho <silviocoutinho@ymail.com>
2023-08-07 11:35:46 -0300

    fix: remove unnecessary import css

--- src/screens/components/SelectDate/index.js
+++ src/screens/components/SelectDate/index.js
@@ -5,7 +5,6 @@ import { Wrapper } from './Styles';
 
 import DatePicker, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import 'react-datepicker/dist/react-datepicker-cssmodules.min.css';
 
 import el from 'date-fns/locale/pt-BR'; // the locale you want
 
