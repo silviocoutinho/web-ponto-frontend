816b5ea888d1f50c59f09925a531e36917f937f1
Silvio  Coutinho <silviocoutinho@ymail.com>
2025-08-21 16:24:35 -0300

    feat: create columns for the table

--- src/screens/Restrito/Usuarios/Gestao/HeaderTableDatabase.js
+++ src/screens/Restrito/Usuarios/Gestao/HeaderTableDatabase.js
@@ -1,12 +1,44 @@
-const HeaderTableDatabase = {
+import BotaoTabela from "./botaoTabela";
+import { handleDelete } from "./handleDelete";
+import { handleEdit } from "./handleEdit";
+import { fields } from '././fields'
+
+const columns =(handlersEdit, handlersDelete)=> {
+
+  const buttons = 
+    {
+    name: "Ações",
+    width: "200px", // largura fixa menor
+    cell: (row) => (
+      
+          <div style={{ display: "flex", gap: "8px" }}>
+            <BotaoTabela 
+              label={"Editar"}
+              color={"orange"}
+              handle={handleEdit}
+              handlers={handlersEdit}
+              row={row}
+            />
+            <BotaoTabela 
+              label={"Desabilitar"}
+              color={"red"}
+              handle={handleDelete}
+              handlers={handlersDelete}
+              row={row}
+            />
+        </div>
+    ),
+    ignoreRowClick: true,
+    allowOverflow: true,
+    button: true,
+  };
+ 
+  return fields.concat(buttons);
   
-  id: 'Cod.',
-  nome: 'Nome',
-  matricula: 'Matr.',
-  pis: 'PIS',
-  ativo: 'Ativo'  
-};
+}
 
 
 
-export { HeaderTableDatabase };
+
+
+export { columns };
---
+++ src/screens/Restrito/Usuarios/Gestao/fields.js
@@ -0,0 +1,29 @@
+const fields = [
+    {  
+    name: 'Cod.',
+    selector: row => row.id,
+    width: "80px", // largura fixa menor
+  },
+  {
+    name: 'Nome',
+    selector: row => row.nome,
+    sortable: true,
+  },
+  {
+    name: 'Matr.',
+    selector: row => row.matricula,
+    sortable: true,
+    width: "120px", // largura fixa menor
+  },
+  {
+    name: 'PIS',
+    selector: row => row.pis,
+  },
+  {
+    name: 'Ativo',
+    selector: row => row.ativo,
+    width: "120px", // largura fixa menor
+  },
+]
+
+export { fields }
