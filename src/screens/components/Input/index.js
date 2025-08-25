4531c5caa7340bf82e1c7387c49c7758955807e7
Silvio  Coutinho <silviocoutinho@ymail.com>
2025-08-25 13:34:09 -0300

    refactor: change file name and remove unnecessary code

--- src/screens/components/Input/index.js
+++ src/screens/components/Input/index.js
@@ -1,6 +1,6 @@
 import {  useState } from 'react';
 import { Form } from 'react-bootstrap';
-import { BsFillBellFill as BsFill } from 'react-icons/bs';
+
 
 import { ComponentStyles } from './Styles';
 
