176d2db1db91ffd5b8fbd4c7a341a21690d28669
Silvio  Coutinho <silviocoutinho@ymail.com>
2025-08-25 11:54:38 -0300

    feat: add TagText in imports

---
+++ src/screens/Restrito/Usuarios/Gestao/imports.js
@@ -0,0 +1,13 @@
+import { ALERT_WARNING, ALERT_DANGER} from '../../../presentation/errors/alertType';
+import { badRequestCode} from '../../../presentation/helpers/http';
+import Input from '../../../components/Input';
+import TagText from '../../../components/TagText';
+
+export {
+  ALERT_WARNING,
+  ALERT_DANGER,
+
+  badRequestCode,
+  
+  Input, TagText
+}
