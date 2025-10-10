e53c89438e697cda5d997ca33806830a657ee93a
Silvio  Coutinho <silviocoutinho@ymail.com>
2025-10-10 17:02:11 -0300

    feat: add function to prepare new record

--- src/screens/Restrito/Usuarios/Gestao/gerarDadosEnvio.js
+++ src/screens/Restrito/Usuarios/Gestao/gerarDadosEnvio.js
@@ -1,5 +1,21 @@
 import { formatDateISO } from "./dataPresentation";
 
+const gerarDadosParaEnvioNovo = (form) => {
+  return {
+    fun_nome: form.nome,
+    fun_data_cadastro: formatDateISO(form.data_cadastro),
+    fun_matricula: form.matricula,   
+    fun_pis: form.pis, 
+    fun_email : form.email,
+    fun_ativo: form.ativo,
+    fun_passwd: form.password,
+    fun_confirmPasswd: form.confirmPasswd,
+    fun_adm: form.adm,
+    fun_usuario: '',
+    fun_senha: 'S3nha@ntiga'
+  }
+}
+
 const gerarDadosParaEnvio = ( 
  form
 ) => {
@@ -17,4 +33,4 @@ const gerarDadosParaEnvio = (
 
 
 
-export { gerarDadosParaEnvio  };
+export { gerarDadosParaEnvioNovo, gerarDadosParaEnvio  };
