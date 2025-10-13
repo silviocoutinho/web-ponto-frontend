e1c0576023e090b6b0846c9bdfb6b8cb8854990b
Silvio  Coutinho <silviocoutinho@ymail.com>
2025-10-13 14:16:40 -0300

    refactor: move objects to another file

---
+++ src/screens/Restrito/Usuarios/Gestao/check/formCheck.js
@@ -0,0 +1,21 @@
+export const formcheckNew = () => {
+  return {
+    nome: 'Nome',
+    data_cadastro: 'Data de Cadastro',
+    matricula: 'Matrícula',   
+    pis: 'PIS', 
+    email : 'E-mail',
+    password: 'Senha',
+    confirmPasswd: 'Confirmar Senha'
+  }
+}
+
+export const formcheckEdit = () => {
+  return {
+    nome: 'Nome',
+    data_cadastro: 'Data de Cadastro',
+    matricula: 'Matrícula',   
+    pis: 'PIS', 
+    email : 'E-mail',
+  }
+}
